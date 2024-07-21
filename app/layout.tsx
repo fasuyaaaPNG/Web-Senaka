2ef14750fa04b4c2713923edb5c3686ff9121407
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-07-21 13:17:10 +0700

    fix ttd

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,9 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/signature_pad/1.3.5/signature_pad.min.js" integrity="sha512-kw/nRM/BMR2XGArXnOoxKOO5VBHLdITAW00aG8qK4zBzcLVZ4nzg7/oYCaoiwc8U9zrnsO9UHqpyljJ8+iqYiQ==" crossOrigin="anonymous" referrerPolicy="no-referrer"></script>
       </head>
       <body className={inter.className}>{children}</body>
-    
     </html>
   );
 }
--- app/presensi/page.tsx
+++ app/presensi/page.tsx
@@ -1,15 +1,49 @@
+"use client";
+
+import { useEffect } from "react";
 import "./style.css"
+import SignaturePad from "signature_pad";
 
 export default function Presensi() {
+
+    useEffect(() => {
+        const canvas = document.getElementById("signature-pad") as HTMLCanvasElement;
+
+        const resizeCanvas = (): void => {
+            const ratio = Math.max(window.devicePixelRatio || 1, 1);
+            canvas.width = canvas.offsetWidth * ratio;
+            canvas.height = canvas.offsetHeight * ratio;
+            const ctx = canvas.getContext("2d");
+            if (ctx) {
+                ctx.scale(ratio, ratio);
+            }
+        };
+
+        window.onresize = resizeCanvas;
+        resizeCanvas();
+
+        const signaturePad = new SignaturePad(canvas, { backgroundColor: 'rgb(250,250,250)' });
+
+        document.getElementById("clear")?.addEventListener('click', () => {
+            signaturePad.clear();
+        });
+
+        // Clean up function
+        return () => {
+            window.onresize = null; // Unregister the resize handler
+        };
+    }, []);
+
     return (
         <div className="background">
             <div className="hiasanBackground"></div>
-            <img draggable={false} src="./assets/presensi/gerbang.svg" alt="" className="gerbang" />
+            <img src="./assets/presensi/gerbang.svg" alt="" className="gerbang" />
             <div className="content">
                 <p className="judul">
                     <span className="judulUtama">PRESENSI SENAKA</span><br /> <span className="judulKanji">セナカ の めんぜん</span>
                 </p>
             </div>
+            <img src="./assets/presensi/sampah.png" id="clear" alt="" className="iconHapus"/>
             <form action="{{ route('presensi.store') }}" name="signature" method="POST">
                 <div className="inputNamaKelasTanggalCam">
                     <fieldset className="Nama">
@@ -31,7 +65,6 @@ export default function Presensi() {
                         <input type="date" required name="tanggal"/>
                     </fieldset>
                     <label className="labelTD" htmlFor="">Tanda tangan: </label>
-                    <img draggable={false} src="./assets/presensi/sampah.png" id="clear" alt="" className="iconHapus"/>
                     <canvas id="signature-pad"></canvas>
                     <input type="hidden" name="foto" id="foto"/>
                 </div>
@@ -39,8 +72,8 @@ export default function Presensi() {
                     Kirim
                 </button>
             </form>
-            <img draggable={false} src="assets/presensi/gambarBelakang.svg" alt="" className="gambarKanan"/>
-            <img draggable={false} src="assets/presensi/gambarBelakang.svg" alt="" className="gambarKiri"/>
+            <img src="assets/presensi/gambarBelakang.svg" alt="" className="gambarKanan"/>
+            <img src="assets/presensi/gambarBelakang.svg" alt="" className="gambarKiri"/>
         </div>       
     )
 }
